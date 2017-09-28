85c69d50e18cd0e54349ef337645a9b54d72c227
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-28 22:28:42 +0200

    Avoid double jump

--- Assets/Project/Scirpts/Player.cs
+++ Assets/Project/Scirpts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
 	public float jumpingForce;
 
+	private bool canJump;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -14,6 +16,11 @@ public class Player : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update ()
+	{
+		ProcessInput ();
+	}
+
+	void ProcessInput ()
 	{
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += Vector3.right * 5f * Time.deltaTime;
@@ -27,7 +34,15 @@ public class Player : MonoBehaviour
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.position += Vector3.back * 5f * Time.deltaTime;
 		}
-		if (Input.GetKeyDown (KeyCode.Space)) {
+
+		// Prevents jump in midair
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, 1.01f)) {
+			canJump = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && canJump) {
+			canJump = false;
 			GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpingForce);
 		} 
 	}
