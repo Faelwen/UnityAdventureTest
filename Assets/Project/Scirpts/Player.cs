fe36b5ad5941791fcc9005770801e0d790d4fefd
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-28 22:58:47 +0200

    Optimisations

--- Assets/Project/Scirpts/Player.cs
+++ Assets/Project/Scirpts/Player.cs
@@ -7,12 +7,13 @@ public class Player : MonoBehaviour
 	public float movingVelocity;
 	public float jumpingVelocity;
 
+	private Rigidbody playerRigidBody;
 	private bool canJump;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		playerRigidBody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -23,19 +24,19 @@ public class Player : MonoBehaviour
 
 	void ProcessInput ()
 	{
-		GetComponent<Rigidbody> ().velocity = new Vector3 (0, GetComponent<Rigidbody> ().velocity.y, 0);
+		playerRigidBody.velocity = new Vector3 (0, playerRigidBody.velocity.y, 0);
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			GetComponent<Rigidbody> ().velocity = new Vector3 (movingVelocity, GetComponent<Rigidbody> ().velocity.y, GetComponent<Rigidbody> ().velocity.z);
+			playerRigidBody.velocity = new Vector3 (movingVelocity, playerRigidBody.velocity.y, playerRigidBody.velocity.z);
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			GetComponent<Rigidbody> ().velocity = new Vector3 (-movingVelocity, GetComponent<Rigidbody> ().velocity.y, GetComponent<Rigidbody> ().velocity.z);
+			playerRigidBody.velocity = new Vector3 (-movingVelocity, playerRigidBody.velocity.y, playerRigidBody.velocity.z);
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody> ().velocity.x, GetComponent<Rigidbody> ().velocity.y, movingVelocity);
+			playerRigidBody.velocity = new Vector3 (playerRigidBody.velocity.x, playerRigidBody.velocity.y, movingVelocity);
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
-			GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody> ().velocity.x, GetComponent<Rigidbody> ().velocity.y, -movingVelocity);
+			playerRigidBody.velocity = new Vector3 (playerRigidBody.velocity.x, playerRigidBody.velocity.y, -movingVelocity);
 		}
 
 		// Prevents jump in midair
@@ -46,7 +47,7 @@ public class Player : MonoBehaviour
 
 		if (Input.GetKeyDown (KeyCode.Space) && canJump) {
 			canJump = false;
-			GetComponent<Rigidbody> ().velocity += Vector3.up * jumpingVelocity;
+			playerRigidBody.velocity = new Vector3 (playerRigidBody.velocity.x, jumpingVelocity, playerRigidBody.velocity.z);
 		} 
 	}
 }
