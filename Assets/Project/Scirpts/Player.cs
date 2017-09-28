b199b883a3dceddfa5f89ce566000dc378a3586c
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-28 23:13:07 +0200

    Make player look around

--- Assets/Project/Scirpts/Player.cs
+++ Assets/Project/Scirpts/Player.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	[Header ("Visuals")]
+	public GameObject model;
+
+	[Header ("Movements")]
 	public float movingVelocity;
 	public float jumpingVelocity;
+	public float rotatingSpeed;
 
 	private Rigidbody playerRigidBody;
+	private Quaternion targetModelRotation;
 	private bool canJump;
 
 	// Use this for initialization
 	void Start ()
 	{
 		playerRigidBody = GetComponent<Rigidbody> ();
+		targetModelRotation = Quaternion.Euler (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		model.transform.rotation = Quaternion.Lerp (model.transform.rotation, targetModelRotation, Time.deltaTime * rotatingSpeed);
 		ProcessInput ();
 	}
 
@@ -26,17 +34,22 @@ public class Player : MonoBehaviour
 	{
 		playerRigidBody.velocity = new Vector3 (0, playerRigidBody.velocity.y, 0);
 
+
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			playerRigidBody.velocity = new Vector3 (movingVelocity, playerRigidBody.velocity.y, playerRigidBody.velocity.z);
+			targetModelRotation = Quaternion.Euler (0, 270, 0);
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			playerRigidBody.velocity = new Vector3 (-movingVelocity, playerRigidBody.velocity.y, playerRigidBody.velocity.z);
+			targetModelRotation = Quaternion.Euler (0, 90, 0);
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			playerRigidBody.velocity = new Vector3 (playerRigidBody.velocity.x, playerRigidBody.velocity.y, movingVelocity);
+			targetModelRotation = Quaternion.Euler (0, 180, 0);
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			playerRigidBody.velocity = new Vector3 (playerRigidBody.velocity.x, playerRigidBody.velocity.y, -movingVelocity);
+			targetModelRotation = Quaternion.Euler (0, 0, 0);
 		}
 
 		// Prevents jump in midair
