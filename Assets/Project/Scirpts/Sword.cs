f54e5029bd7dea328a5cea1b88263da320993f36
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-29 20:09:18 +0200

    Swing sword back

--- Assets/Project/Scirpts/Sword.cs
+++ Assets/Project/Scirpts/Sword.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
 
-	public float swingingSpeed = 2f;
+	public float swingingSpeed = 5f;
+	public float cooldownSpeed = 2f;
+	public float attackDuration = 0.5f;
+	public float cooldownDuration = 0.5f;
 
 	private Quaternion targetRotation;
+	private float cooldownTimer;
 
 	// Use this for initialization
 	void Start ()
@@ -19,11 +23,23 @@ public class Sword : MonoBehaviour
 	void Update ()
 	{
 		transform.localRotation = Quaternion.Lerp (transform.localRotation, targetRotation, Time.deltaTime * swingingSpeed);
+		cooldownTimer -= Time.deltaTime;
 	}
 
 
 	public void Attack ()
 	{
+		if (cooldownTimer > 0f) {
+			return;
+		}
 		targetRotation = Quaternion.Euler (-90, 0, 0);
+		cooldownTimer = cooldownDuration;
+		StartCoroutine (CooldownWait ());
+	}
+
+	private IEnumerator CooldownWait ()
+	{
+		yield return new WaitForSeconds (attackDuration);
+		targetRotation = Quaternion.Euler (0, 0, 0);
 	}
 }
