62544e5be608e42a9d63bd0c88e89e46eb149649
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-29 23:07:04 +0200

    Exploding bomb

--- Assets/Project/Scirpts/Bomb.cs
+++ Assets/Project/Scirpts/Bomb.cs
@@ -7,21 +7,38 @@ public class Bomb : MonoBehaviour
 
 	public float duration = 5f;
 	public float explosionRadius = 3f;
+	public float explosionDuration = 0.25f;
+	public GameObject explosionModel;
 
 	private float explosionTimer;
+	private bool exploded;
 
 	// Use this for initialization
 	void Start ()
 	{
 		explosionTimer = duration;
+		explosionModel.transform.localScale = Vector3.one * explosionRadius;
+		explosionModel.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		explosionTimer -= Time.deltaTime;
-		if (explosionTimer <= 0) {
-			Destroy (gameObject);
+		if (explosionTimer <= 0 && exploded == false) {
+			exploded = true;
+			Collider[] hitObjects = Physics.OverlapSphere (transform.position, explosionRadius);
+			foreach (Collider collider in hitObjects) {
+				Debug.Log (collider.name);
+			}
+			StartCoroutine (Explode ());
 		}
+	}
+
+	private IEnumerator Explode ()
+	{
+		explosionModel.SetActive (true);
+		yield return new WaitForSeconds (explosionDuration);
+		Destroy (gameObject);
 	}
 }
