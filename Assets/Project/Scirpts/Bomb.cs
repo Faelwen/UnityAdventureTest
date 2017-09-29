2b6f518da5ee235bac1e8f254ae7e987ad9cc6e6
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-29 22:52:55 +0200

    Bomb script inits

---
+++ Assets/Project/Scirpts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+	public float duration = 5f;
+	public float explosionRadius = 3f;
+
+	private float explosionTimer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		explosionTimer = duration;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		explosionTimer -= Time.deltaTime;
+		if (explosionTimer <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
