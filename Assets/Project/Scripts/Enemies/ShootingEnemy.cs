dbd1cbaaab3a99380e24e29ce365398ffddaedab
Faelwen <francis.john.mccarthy@googlemail.com>
2017-10-02 21:32:29 +0200

    Simple enemy script

--- Assets/Project/Scripts/Enemies/ShootingEnemy.cs
+++ Assets/Project/Scripts/Enemies/ShootingEnemy.cs
@@ -6,16 +6,27 @@ public class ShootingEnemy : Enemy
 {
 
 	public GameObject Model;
+	public float timeToRotate = 2f;
+	public float rotationSpeed = 6f;
+
+	private int targetAngle;
+	private float rotationTimer;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		rotationTimer = timeToRotate;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		rotationTimer -= Time.deltaTime;
+		if (rotationTimer <= 0f) {
+			rotationTimer = timeToRotate;
+			targetAngle += 90;
+		}
+
+		transform.localRotation = Quaternion.Lerp (transform.localRotation, Quaternion.Euler (0, targetAngle, 0), Time.deltaTime * rotationSpeed);
 	}
 }
