c7c61ae17031a55f5d0d65c808b50b512b69dc50
Faelwen <francis.john.mccarthy@googlemail.com>
2017-10-02 21:25:56 +0200

    Add shooting enemy

---
+++ Assets/Project/Scripts/Enemies/ShootingEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : Enemy
+{
+
+	public GameObject Model;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
