1757d388ad73e7f6d798195859c4d5c5e06de4fb
Faelwen <francis.john.mccarthy@googlemail.com>
2017-10-04 22:30:07 +0200

    Basic UI creation

---
+++ Assets/Project/Scripts/GameScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScene : MonoBehaviour
+{
+	[Header ("Game")]
+	public Player player;
+
+	[Header ("UI")]
+	public Text healthText;
+	public Text bombText;
+	public Text arrowText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player != null) {
+			healthText.text = "Health: " + player.health;
+			bombText.text = "Bombs: " + player.bombAmount;
+			arrowText.text = "Arrows: " + player.arrowAmount;
+		} else {
+			healthText.text = "Health: 0";
+		}
+	}
+}
